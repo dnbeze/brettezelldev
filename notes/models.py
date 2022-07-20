c9731375d5c7d47891d7e3b35fe5c492f1099df5
Brett Ezell <dnbeze55@gmail.com>
2022-07-19 21:01:37 -0400

    this is actually working. Post side restore cool no idea what happened will have to check it out.

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Reminder(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
